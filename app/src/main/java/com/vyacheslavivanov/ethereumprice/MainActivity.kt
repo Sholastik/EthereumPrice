aa82255826207580171fa5a1270d8acba690caba
vyacheslavivanov <persikchicken@icloud.com>
2022-07-29 20:41:29 +0500

    Set up splash screen

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/MainActivity.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.vyacheslavivanov.ethereumprice
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.vyacheslavivanov.ethereumprice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Should be run before super
+        installSplashScreen() // SplashScreen won't show when run via Android Studio: https://issuetracker.google.com/issues/205021357
+
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
     }
