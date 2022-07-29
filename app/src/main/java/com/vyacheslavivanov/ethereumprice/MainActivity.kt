6c125eee27663438f1d1e3e00bd415e18e1564c2
vyacheslavivanov <persikchicken@icloud.com>
2022-07-29 20:45:32 +0500

    Added NavHostFragment to MainActivity

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/MainActivity.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.vyacheslavivanov.ethereumprice
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.FragmentActivity
 import com.vyacheslavivanov.ethereumprice.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
