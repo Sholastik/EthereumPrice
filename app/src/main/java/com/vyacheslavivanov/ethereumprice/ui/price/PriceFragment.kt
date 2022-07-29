9a30e9c4c144f946379c472c2f8030df3fcd4733
vyacheslavivanov <persikchicken@icloud.com>
2022-07-29 20:51:17 +0500

    Created PriceFragment

---
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceFragment.kt
@@ -0,0 +1,22 @@
+package com.vyacheslavivanov.ethereumprice.ui.price
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.vyacheslavivanov.ethereumprice.databinding.FragmentPriceBinding
+
+class PriceFragment : Fragment() {
+    private lateinit var binding: FragmentPriceBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPriceBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
