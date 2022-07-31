c8344324a55e15114dd672c0f616a6ee317cf095
vyacheslavivanov <persikchicken@icloud.com>
2022-07-31 18:44:34 +0500

    Made PriceFragment use Jetpack Compose

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceFragment.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceFragment.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
-import com.vyacheslavivanov.ethereumprice.databinding.FragmentPriceBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PriceFragment : Fragment() {
-    private lateinit var binding: FragmentPriceBinding
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentPriceBinding.inflate(inflater, container, false)
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
 
-        return binding.root
+            }
+        }
     }
 }
