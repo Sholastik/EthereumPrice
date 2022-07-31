d8ba2075d1d88e1a296f65db2494047ef386e712
vyacheslavivanov <persikchicken@icloud.com>
2022-07-31 19:17:21 +0500

    Created PriceDateCard composable

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceScreen.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceScreen.kt
@@ -3,16 +3,22 @@ package com.vyacheslavivanov.ethereumprice.ui.price
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vyacheslavivanov.ethereumprice.R
+import com.vyacheslavivanov.ethereumprice.data.price.Price
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Composable
 fun PriceScreen() {
@@ -46,3 +52,41 @@ fun PriceScreenTitlePreview() {
     }
 }
 
+@Composable
+fun PriceDateCard(modifier: Modifier = Modifier, price: Price) {
+    Card(
+        modifier = modifier,
+        backgroundColor = Color(0xFFF2F3F5),
+        shape = RoundedCornerShape(10.dp)
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+            text = when (price) {
+                is Price.Live -> stringResource(id = R.string.price_card_date_now)
+                is Price.Historical -> SimpleDateFormat(
+                    stringResource(id = R.string.price_card_date_format),
+                    Locale.getDefault()
+                ).format(price.date)
+            },
+            textAlign = TextAlign.Center,
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF
+)
+@Composable
+fun PriceDateCardPreview() {
+    Column {
+        PriceDateCard(
+            modifier = Modifier.padding(16.dp),
+            price = Price.Live(
+                100.0
+            )
+        )
+    }
+}
