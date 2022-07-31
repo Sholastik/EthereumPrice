d0bb9e957a9dc0f5ad6ab0788e74a8756d0ff753
vyacheslavivanov <persikchicken@icloud.com>
2022-07-31 17:53:42 +0500

    Created PriceMappers

---
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/api/mappers/PriceMappers.kt
@@ -0,0 +1,7 @@
+package com.vyacheslavivanov.ethereumprice.api.mappers
+
+import com.vyacheslavivanov.ethereumprice.api.dto.price.LivePriceResponse
+import com.vyacheslavivanov.ethereumprice.data.price.Price
+
+fun LivePriceResponse.toDomain(): Price =
+    Price(price = price)
