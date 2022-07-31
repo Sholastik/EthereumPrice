fde32d9495c921d4550c38607cbe27432d63e2e6
vyacheslavivanov <persikchicken@icloud.com>
2022-07-31 22:10:34 +0500

    Created PriceText

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceScreen.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceScreen.kt
@@ -1,18 +1,21 @@
 package com.vyacheslavivanov.ethereumprice.ui.price
 
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -143,6 +146,51 @@ fun PriceDateSelectButtonPreview() {
     Column {
         PriceDateSelectButton(
             modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Composable
+fun PriceText(
+    modifier: Modifier = Modifier,
+    price: Price
+) {
+    val style = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    )
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "1",
+            style = style
+        )
+        Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_ethereum_logo),
+            contentDescription = null,
+        )
+        Text(
+            text = "= ${String.format("%.2f", price.price)} $",
+            style = style
+        )
+    }
+}
+
+@Preview(
+    showBackground = true,
+    backgroundColor = 0xFFFFFFFF
+)
+@Composable
+fun PriceTextPreview() {
+    Column {
+        PriceText(
+            modifier = Modifier.padding(16.dp),
+            price = Price.Live(
+                price = 100.0
+            )
         )
     }
 }
