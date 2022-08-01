a7d60d8c5cae2cc70fd2ed8fd02cc33105b630b8
vyacheslavivanov <persikchicken@icloud.com>
2022-08-01 20:08:02 +0500

    Fixed UI error

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceDateSheet.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceDateSheet.kt
@@ -1,11 +1,14 @@
 package com.vyacheslavivanov.ethereumprice.ui.price
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ModalBottomSheetLayout
 import androidx.compose.material.ModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -30,7 +33,7 @@ fun PriceDateSheetLayout(
 
 @Composable
 fun PriceDateSheetContent() {
-
+    Box(modifier = Modifier.height(256.dp))
 }
 
 @Preview(
