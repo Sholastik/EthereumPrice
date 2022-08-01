b90c6b5e18c5b843496a058802f2c9063a847f15
vyacheslavivanov <persikchicken@icloud.com>
2022-08-01 17:16:51 +0500

    Fixed DI errors

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/data/price/PriceRepository.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/data/price/PriceRepository.kt
@@ -3,13 +3,14 @@ package com.vyacheslavivanov.ethereumprice.data.price
 import com.vyacheslavivanov.ethereumprice.api.source.price.LivePriceSource
 import com.vyacheslavivanov.ethereumprice.data.Resource
 import com.vyacheslavivanov.ethereumprice.data.Resource.Companion.toResource
+import com.vyacheslavivanov.ethereumprice.di.PriceApiModule
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class PriceRepository @Inject constructor(
-    private val livePriceSource: LivePriceSource
+    @PriceApiModule.PriceApi private val livePriceSource: LivePriceSource
 ) {
     val priceFlow by lazy {
         livePriceFlow
@@ -17,7 +18,7 @@ class PriceRepository @Inject constructor(
 
     private val livePriceFlow: Flow<Resource<Price.Live>> = flow {
         while (true) {
-            val resource = livePriceSource.fetchPrice().toResource()
+            val resource = livePriceSource.fetchLivePrice().toResource()
             emit(resource)
             delay(1000)
         }
