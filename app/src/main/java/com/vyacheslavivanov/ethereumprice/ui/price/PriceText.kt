4ce1f2039be9a485bf8ece042f97fa260f83c7c7
vyacheslavivanov <persikchicken@icloud.com>
2022-08-01 17:49:27 +0500

    Added live badge

--- app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceText.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/ui/price/PriceText.kt
@@ -7,6 +7,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -16,6 +20,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.vyacheslavivanov.ethereumprice.R
 import com.vyacheslavivanov.ethereumprice.data.Resource
 import com.vyacheslavivanov.ethereumprice.data.price.Price
@@ -30,36 +35,80 @@ fun PriceText(
         fontSize = 24.sp
     )
 
-    Row(
-        modifier = modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ) {
-        when (price) {
-            is Resource.Success -> {
-                Text(
-                    text = "1",
-                    style = style
-                )
-                Image(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_ethereum_logo),
-                    contentDescription = null,
-                )
-                Text(
-                    text = "= ${String.format("%.2f", price.data.price)} $",
-                    style = style
-                )
+    when (price) {
+        is Resource.Success -> {
+            ConstraintLayout(modifier = modifier.fillMaxWidth()) {
+                val (label, badge) = createRefs()
+
+                if (price.data is Price.Live) {
+                    Text(
+                        text = stringResource(id = R.string.price_text_live_badge_title),
+                        modifier = Modifier
+                            .constrainAs(badge) {
+                                baseline.linkTo(label.baseline, margin = 8.dp)
+                                start.linkTo(label.end, margin = 4.dp)
+                                top.linkTo(parent.top)
+                            }
+                            .drawBehind {
+                                drawRoundRect(
+                                    brush = Brush.linearGradient(
+                                        colors = listOf(
+                                            Color(0xFFFC1E7B),
+                                            Color(0xFFFA3349)
+                                        )
+                                    ),
+                                    cornerRadius = CornerRadius(
+                                        x = 4.dp.toPx(),
+                                        y = 4.dp.toPx()
+                                    )
+                                )
+                            }
+                            .padding(horizontal = 8.dp, vertical = 4.dp),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White
+                    )
+                }
+
+                Row(
+                    modifier = Modifier.constrainAs(label) {
+                        bottom.linkTo(parent.bottom)
+                        start.linkTo(parent.start)
+                        end.linkTo(parent.end)
+                    },
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(
+                        text = "1",
+                        style = style
+                    )
+                    Image(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_ethereum_logo),
+                        contentDescription = null,
+                    )
+                    Text(
+                        text = "= ${String.format("%.2f", price.data.price)} $",
+                        style = style
+                    )
+                }
             }
-            is Resource.Loading -> {
+        }
+        is Resource.Loading -> {
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
                 CircularProgressIndicator()
             }
-            is Resource.Error -> {
-                Text(
-                    stringResource(id = R.string.price_screen_exception_title),
-                    style = style,
-                    textAlign = TextAlign.Center
-                )
-            }
+        }
+        is Resource.Error -> {
+            Text(
+                modifier = modifier.fillMaxWidth(),
+                text = stringResource(id = R.string.price_screen_exception_title),
+                style = style,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
