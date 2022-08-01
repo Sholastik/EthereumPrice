74e4bd652e42ec858ae774f157e87f66b1271ab7
vyacheslavivanov <persikchicken@icloud.com>
2022-08-01 18:33:38 +0500

    Created HistoricalPriceResponse

---
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/api/dto/price/HistoricalPriceResponse.kt
@@ -0,0 +1,21 @@
+package com.vyacheslavivanov.ethereumprice.api.dto.price
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import java.util.*
+
+@JsonClass(generateAdapter = true)
+data class HistoricalPriceResponse(
+    @Json(name = "Data") val data: Data
+) {
+    @JsonClass(generateAdapter = true)
+    data class Data(
+        @Json(name = "Data") val data: List<PriceResponse>
+    ) {
+        @JsonClass(generateAdapter = true)
+        data class PriceResponse(
+            @Json(name = "close") val price: Double,
+            @Json(name = "timestamp") val date: Date
+        )
+    }
+}
--- app/src/main/java/com/vyacheslavivanov/ethereumprice/api/mappers/PriceMappers.kt
+++ app/src/main/java/com/vyacheslavivanov/ethereumprice/api/mappers/PriceMappers.kt
@@ -1,7 +1,16 @@
 package com.vyacheslavivanov.ethereumprice.api.mappers
 
+import com.vyacheslavivanov.ethereumprice.api.dto.price.HistoricalPriceResponse
 import com.vyacheslavivanov.ethereumprice.api.dto.price.LivePriceResponse
 import com.vyacheslavivanov.ethereumprice.data.price.Price
 
 fun LivePriceResponse.toDomain(): Price.Live =
     Price.Live(price)
+
+fun HistoricalPriceResponse.toDomain(): Price.Historical =
+    data.data.last().run {
+        Price.Historical(
+            price = price,
+            date = date
+        )
+    }
